210a5c1ced6b80e60e35e69264685c3c7ee78a63
viky <viky@G580-viky>
2013-09-21 18:04:40 +0200

    launch settings can be edited

--- app/model/ServerRepository.php
+++ app/model/ServerRepository.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace DB;
 
 /**
@@ -6,5 +7,46 @@ namespace DB;
  * @author viky
  */
 class ServerRepository extends Repository {
-    
+
+    /**
+     * @param int $serverId
+     * @param string $name
+     * @param string $path
+     * @param string $executable
+     * @return boolean
+     * @throws PDOException
+     */
+    public function updateServerParams($serverId, $name, $path, $executable) {
+        try {
+            $this->getTable()->where('id', $serverId)->update(array(
+                'name' => $name,
+                'path' => $path,
+                'executable' => $executable
+            ));
+            return TRUE;
+        } catch (PDOException $e) {
+            throw new PDOException;
+        }
+    }
+    /**
+     * sets runtime has for running server identification
+     * @param int $serverId
+     * @param string $hash
+     * @return boolean
+     * @throws PDOException
+     */
+    public function setRuntimeHash($serverId, $hash) {
+        try {
+            $this->getTable()->where('id', $serverId)->update(array(
+                'hash' => $hash
+            ));
+            return TRUE;
+        } catch (PDOException $e) {
+            throw new PDOException;
+        }
+    }
+    public function generateRuntimeHash(){
+        return \Nette\Utils\Strings::random();
+    }
+
 }
--- app/presenters/GameSettingsPresenter.php
+++ app/presenters/GameSettingsPresenter.php
@@ -1,10 +1,55 @@
 <?php
 
+use Nette\Application\UI\Form;
+
 /**
  * Description of GameSettingsPresenter
  *
  * @author viky
  */
-class GameSettingsPresenter extends SecuredPresenter{
-    //put your code here
+class GameSettingsPresenter extends SecuredPresenter {
+
+    /** @var DB\ServerRepository */
+    private $serverRepo;
+
+    protected function startup() {
+        parent::startup();
+        $this->serverRepo = $this->context->serverRepository;
+    }
+
+    /**
+     * @return Nette\Application\UI\Form
+     */
+    protected function createComponentServerParams() {
+        $form = new Form();
+        $form->addGroup('runtime');
+        $form->addText('name', 'Jméno: ', 30, 20)
+                ->addRule(Form::FILLED, 'server musí mít jméno');
+        $form->addText('path', 'Cesta: ', 30)
+                ->addRule(Form::FILLED, 'je nutné specifikovat cestu');
+        $form->addText('executable', 'jméno .jar: ', 30)
+                ->addRule(Form::FILLED, 'je nutno specifikovat jméno .jar souboru');
+        $form->addSubmit('update', 'Upravit');
+        //defaults
+        $values = $this->serverRepo->findById($this->selectedServerId)->fetch();
+        $form->setValues($values);
+        $form->onSuccess[] = $this->serverParamsFormSubmitted;
+        return $form;
+    }
+
+    /**
+     * @param \Nette\Application\UI\Form $form
+     */
+    public function serverParamsFormSubmitted(Form $form) {
+        $values = $form->getValues();
+        if (substr($values->path, -1) != '/') {
+            $values->path .= '/';
+        }
+        $this->serverRepo->updateServerParams(
+                $this->selectedServerId, $values->name, $values->path, $values->executable
+                );
+        $this->flashMessage('Nastavení aktualizováno.', 'success');
+        $this->redirect('this');
+    }
+
 }
