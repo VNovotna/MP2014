0a0ab4a19964342eaa2a8a2652685febff7151ec
viky <viky@G580-viky>
2013-10-05 23:17:41 +0200

    LogModel (not actually a model by conventions) read all server log files Finder is \Nette\Utils\Finder with sorting

---
+++ app/model/LogModel.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Class for reading log files
+ * 
+ * @author viky
+ */
+class LogModel extends \Nette\Object{
+
+    /**
+     * @var Finder
+     */
+    protected $finder;
+
+    public function __construct($filePath, $fileMask = 'server.log*', $exclude = '*lck') {
+        $this->finder = Finder::findFiles($fileMask)->exclude($exclude)->in($filePath);
+    }
+
+    public function getAll() {
+        $i = 0;
+        $output = array();
+        foreach ($this->finder->orderByMTime() as $value) {
+            $output = array_merge($output, array_reverse(file($value)));
+            $i++;
+        }
+        return $output;
+    }
+
+    public function getWarnings() {
+        
+    }
+
+    public function getInfos() {
+        
+    }
+
+}
