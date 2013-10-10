63d2784df807086c6ca80a96d57d8371e044cf9b
viky <viky@G580-viky>
2013-10-10 16:14:21 +0200

    backup()

--- app/model/ServerCommander.php
+++ app/model/ServerCommander.php
@@ -24,7 +24,7 @@ class ServerCommander extends \Nette\Object {
      * @param string $jarPath absolute path to server folder
      * @param string $jarName name of server .jar
      * @param string $runtimeHash default 'mcs', usefull with more than one server
-     * @return array usually empty if command was successfull
+     * @return array usually empty array if command was successfull
      * @throws \Nette\InvalidStateException
      */
     public function startServer($jarPath, $jarName, $runtimeHash) {
@@ -34,7 +34,11 @@ class ServerCommander extends \Nette\Object {
         } else {
             $exec = './../libs/start.sh ' . $jarPath . ' ' . $jarName . ' ' . $runtimeHash;
             exec($exec, $output);
-            return $output;
+            if ($this->isServerRunning($runtimeHash)) {
+                return $output;
+            } else {
+                return array('something somewhere went terribly wrong');
+            }
         }
     }
 
@@ -59,6 +63,28 @@ class ServerCommander extends \Nette\Object {
             return TRUE;
         }
         return FALSE;
+    }
+
+    /**
+     * Backup world/ folder. Provide runtime hash when you want to save running server
+     * @param string $path path to folder with minecraft
+     * @param string $runtimeHash
+     * @return boolean
+     */
+    public function backup($path, $runtimeHash) {
+        if ($this->isServerRunning($runtimeHash)) {
+            echo "saving running";
+            $date = new Nette\DateTime();
+            $filename = $date->format('Y-m-d_H:i');
+            $phar = new PharData('/home/viky/mcs/backups/' . $filename . '.zip');
+            $this->issueCommand('save-all', $runtimeHash);
+            $this->issueCommand('save-off', $runtimeHash);
+            $phar->buildFromDirectory('/home/viky/mcs/world');
+            $this->issueCommand('save-on', $runtimeHash);
+            return TRUE;
+        } else {
+            throw new Nette\NotImplementedException;
+        }
     }
 
 }
