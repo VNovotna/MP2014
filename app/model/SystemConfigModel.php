370caa4d8ef6cb1f75acdaf1e41934a4f3301cb6
viky <viky@G580-V>
2014-02-08 17:33:10 +0100

    SystemSettings: mockup

--- app/model/SystemConfigModel.php
+++ app/model/SystemConfigModel.php
@@ -16,14 +16,27 @@ class SystemConfigModel extends Nette\Object {
         $file = implode('', file('../app/config/app.neon'));
         $this->neon = Utils\Neon::decode($file);
     }
+
     /**
      * @param string $section
      * @return array
      */
     public function getConfig($section = NULL) {
-        if ($section == NULL)
+        if ($section == NULL) {
             return $this->neon;
-        else {
+        } else {
+            return $this->neon[$section];
+        }
+    }
+
+    /**
+     * @param string $section
+     */
+    public function setConfig($section = NULL) {
+        throw new Nette\NotImplementedException;
+        if ($section == NULL) {
+            return $this->neon;
+        } else {
             return $this->neon[$section];
         }
     }
--- app/presenters/SystemSettingsPresenter.php
+++ app/presenters/SystemSettingsPresenter.php
@@ -1,5 +1,7 @@
 <?php
 
+use Nette\Application\UI\Form;
+
 /**
  * Description of SystemSettingsPresenter
  *
@@ -15,8 +17,94 @@ class SystemSettingsPresenter extends SecuredPresenter {
         $this->configModel = $this->context->systemConfigModel;
     }
 
+    /**
+     * @return \Nette\Application\UI\Form
+     */
+    protected function createComponentSettingsUpdate() {
+        $form = new Form();
+        $form->addGroup('aktualizace');
+        $form->addText('number', "Max. počet uložených .jar:")
+                ->setAttribute('type', 'number')
+                ->addRule(Form::INTEGER, "Hodnota musí být číslo.");
+        $form->addRadioList('foreignBool', "Povolit nahrávat vlastní .jar:", array(FALSE=>'FALSE',TRUE=>'TRUE'));
+        $form->addTextArea('url', 'Zdroje aktualizací:',36,3);
+        $form->addTextArea('regex', 'Parsovací regex:',36,3);
+        $form->addSubmit('send', 'Uložit');
+        $form->setDefaults($this->configModel->getConfig('update'));
+        $form->onSuccess[] = $this->settingsUpdateSubmitted;
+        return $form;
+    }
+
+    /**
+     * @param \Nette\Application\UI\Form $form
+     */
+    public function settingsUpdateSubmitted($form) {
+        
+    }
+   /**
+     * @return \Nette\Application\UI\Form
+     */
+    protected function createComponentSettingsBackup() {
+        $form = new Form();
+        $form->addGroup('zálohy');
+        $form->addText('number', "Max. počet uložených záloh:")
+                ->setAttribute('type', 'number')
+                ->addRule(Form::INTEGER, "Hodnota musí být číslo.");
+        $form->addRadioList('foreignBool', "Povolit nahrávat vlastních záloh:", array(TRUE=>'TRUE', FALSE=>'FALSE'));
+        $form->addSubmit('send', 'Uložit');
+        $form->setDefaults($this->configModel->getConfig('backup'));
+        $form->onSuccess[] = $this->settingsBackupSubmitted;
+        return $form;
+    }
+
+    /**
+     * @param \Nette\Application\UI\Form $form
+     */
+    public function settingsBackupSubmitted($form) {
+        
+    }
+       /**
+     * @return \Nette\Application\UI\Form
+     */
+    protected function createComponentSettingsStorage() {
+        $form = new Form();
+        $form->addGroup('úložiště');
+        $form->addText('common', "Úložistě herních dat:")
+                ->addRule(Form::FILLED, "Musíte vyplnit 0 nebo cestu v systému");
+        $form->addSubmit('send', 'Uložit');
+        $form->setDefaults($this->configModel->getConfig('storage'));
+        $form->onSuccess[] = $this->settingsStorageSubmitted;
+        return $form;
+    }
+
+    /**
+     * @param \Nette\Application\UI\Form $form
+     */
+    public function settingsStorageSubmitted($form) {
+        
+    }       /**
+     * @return \Nette\Application\UI\Form
+     */
+    protected function createComponentSettingsServer() {
+        $form = new Form();
+        $form->addGroup('MC server');
+        $form->addText('number', "Max. počet serverů na hráče:")
+                ->setAttribute('type', 'number')
+                ->addRule(Form::INTEGER, "Hodnota musí být číslo.");
+        $form->addSubmit('send', 'Uložit');
+        $form->setDefaults($this->configModel->getConfig('server'));
+        $form->onSuccess[] = $this->settingsServerSubmitted;
+        return $form;
+    }
+
+    /**
+     * @param \Nette\Application\UI\Form $form
+     */
+    public function settingsServerSubmitted($form) {
+        
+    }
     public function renderDefault() {
-        dump($this->configModel->getConfig());
+        //dump($this->configModel->getConfig());
     }
 
 }
