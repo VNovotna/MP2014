da5422af45397758128cc6cef5bf4e1a176ab988
viky <viky@G580-V>
2014-03-10 20:26:36 +0100

    UUIDModel - some new Exceptions and docs

--- app/model/UUIDModel.php
+++ app/model/UUIDModel.php
@@ -7,18 +7,35 @@
  */
 class UUIDModel extends \Nette\Object {
 
+    /**
+     * @param string $userName
+     * @return string uuid of $usrName
+     * @throws ProfileNotUniqueException
+     * @throws ProfileNotFoundException
+     */
     public function getUuid($userName) {
         $data = $this->makeRequest($userName);
         if ($data->size > 1) {
             throw new ProfileNotUniqueException;
+        }if ($data->size == 0) {
+            throw new ProfileNotFoundException;
         }
         return $data->profiles[0]->id;
     }
 
+    /**
+     * @param string $userName
+     * @throws Nette\NotImplementedException
+     */
     public function getUuids($userName) {
         throw new Nette\NotImplementedException;
     }
 
+    /**
+     * @param string $userName
+     * @return mixed from json_decode
+     * @throws Nette\FileNotFoundException
+     */
     private function makeRequest($userName) {
         $url = 'https://api.mojang.com/profiles/page/1';
         $options = array(
@@ -29,12 +46,19 @@ class UUIDModel extends \Nette\Object {
             ),
         );
         $context = stream_context_create($options);
-        $result = file_get_contents($url, false, $context);
-        return json_decode($result);
+        $result = @file_get_contents($url, false, $context);
+        if ($result !== FALSE) {
+            return json_decode($result);
+        }
+        throw new Nette\FileNotFoundException('Network unreachable');
     }
 
 }
 
 class ProfileNotUniqueException extends Exception {
+    
+}
+
+class ProfileNotFoundException extends Exception {
     
 }
